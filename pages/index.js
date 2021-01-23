72db252343d2c574acce8f1541519059366f1114
pai <69026987+ELPanaJose@users.noreply.github.com>
2021-01-23 10:04:12 -0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi! my name is Jose I'm a junior developer learning new tecnologies.{" "}
+          <a href="https://github.com/ELPanaJose">My GitHub Profile</a>.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
